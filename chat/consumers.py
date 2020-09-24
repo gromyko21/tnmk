97475c6673bf1b77205069fd02def22f9fa5dc22
Aleksandr <gromyko2909@mail.ru>
2020-09-24 14:28:00 +0500

    Изменения в списках чатов

--- chat/consumers.py
+++ chat/consumers.py
@@ -69,10 +69,10 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-        chat_id = get_object_or_404(Chat, id=self.room_name)
-        messages = Message.objects.order_by('pk').filter(recipient=chat_id)
-        self.user_name = 1
-        self.user = 'user_%s' % self.user_name
+
+
+        # self.user_name = members
+        self.user = 'user_2'
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
